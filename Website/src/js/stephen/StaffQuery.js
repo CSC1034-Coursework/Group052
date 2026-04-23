ac8d5af143a549b8cb121d96868245681cd1d660
SackTNT <BruhFunnyMoment@hotmail.com>
2026-04-24 00:29:47 +0100

    Update StaffQuery.js
    Added loadRegions for dropdown table

--- Website/src/js/stephen/StaffQuery.js
+++ Website/src/js/stephen/StaffQuery.js
@@ -1,12 +1,37 @@
 console.log("StaffQuery loaded");
+console.log("test loaded");
+
+const loadRegions = async () => {
+        const sql = `SELECT regionID, regionName 
+        FROM tblRegion 
+        ORDER BY regionName;`;
+        const select = document.getElementById("regionFilter");
+
+        try {
+          const result = await runQuery(sql);
+
+          if (!result || !result.data) return;
+
+          result.data.forEach(region => {
+            const option = document.createElement("option");
+            option.value = region.regionID; 
+            option.textContent = region.regionName;
+            select.appendChild(option);
+          });
+
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
 const runStaffRegionQuery = async () => {
     const region = document.getElementById("regionFilter").value;
     const sql = `SELECT r.regionName, COUNT(s.staffID) AS NumberOfStaff
-    FROM tblStaff s
-    JOIN tblRegion r ON s.regionID = r.regionID
-    ${region ? `WHERE r.regionName = '${region}'` : ""}
-    GROUP BY r.regionName;`;
+    FROM tblRegion r
+    LEFT JOIN tblStaff s ON s.regionID = r.regionID
+    ${region ? `WHERE r.regionID = ${region}` : ""}
+    GROUP BY r.regionName
+    ORDER BY r.regionName ASC;`;
     // If region is there, display that region row, else display nothing
     const output = document.getElementById("outputStaff");
     const url = "https://sbrown635.webhosting1.eeecs.qub.ac.uk/dbConnector.php";
@@ -36,6 +61,7 @@ const runStaffRegionQuery = async () => {
         const table = document.createElement("table");
         const headerRow = document.createElement("tr");
         table.appendChild(headerRow);
+        console.log(region);
 
         const headings = ["Region Name", "Number of Staff"];
 
@@ -67,6 +93,7 @@ const runStaffRegionQuery = async () => {
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
+    await loadRegions();
     document.getElementById("regionFilter").addEventListener("change", runStaffRegionQuery);
-    await runStaffRegionQuery();
+    runStaffRegionQuery();
 });
